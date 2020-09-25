5d39ac06332495421aebb4b251d0c60dcf219318
Diego Sevilla <sevilla.diego8@gmail.com>
2020-09-24 19:32:04 -0600

    theEnd(): clean comments and updated readme

--- methods.py
+++ methods.py
@@ -249,29 +249,29 @@ class XMPP_Client(sleekxmpp.ClientXMPP):
         except IqTimeout:
             raise Exception("No response from server.")
 
-    '''def showContactsList(self):
+    def showContactsList(self):
         try:
             self.get_roster()
-	    except IqError as e:
-	        print('Error: %s' % e.iq['error']['condition'])
-	    except IqTimeout:
-	        print('No response from server.')
-	    groups = self.client_roster.groups()
-		for group in groups:
-			print('\n%s' % group)
-	    	print('-' * 50)
-			for jid in groups[group]:
-			    sub = self.client_roster[jid]['subscription']
-			    name = self.client_roster[jid]['name']
-				if self.client_roster[jid]['name']:
-					print(' %s (%s) [%s]' % (name, jid, sub))
-				else:
-					print(' %s [%s]' % (jid, sub))
-				connections = self.client_roster.presence(jid)
-				for res, pres in connections.items():
-					show = 'available'
-					if pres['status']:
-						print('       %s' % pres['status'])'''
+        except IqError as e:
+            print('Error: %s' % e.iq['error']['condition'])
+        except IqTimeout:
+            print('No response from server.')
+        groups = self.client_roster.groups()
+        for group in groups:
+            print('\n%s' % group)
+            print('-' * 50)
+            for jid in groups[group]:
+                sub = self.client_roster[jid]['subscription']
+                name = self.client_roster[jid]['name']
+                if self.client_roster[jid]['name']:
+                    print(' %s (%s) [%s]' % (name, jid, sub))
+                else:
+                    print(' %s [%s]' % (jid, sub))
+                connections = self.client_roster.presence(jid)
+                for res, pres in connections.items():
+                    show = 'available'
+                    if pres['status']:
+                        print('       %s' % pres['status'])
 
     #Check for a user
     def checkUser(self, jid):
